13f28bd8167c7999cb941dc1c8af482dbf2c85e7
elboaf <jesse.wahl@gmail.com>
2016-10-27 08:04:01 -0700

    Update cryptopals_s1_c1.py

--- cryptopals_s1_c1.py
+++ cryptopals_s1_c1.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+# http://cryptopals.com/sets/1/challenges/1
+# import the library to do the operations because we arent into re-inventing the wheel just yet.
 import binascii
+# set static variable with our challenge input
 mh = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+# print the answer using binascii functions unhexlify and b2a_base64
+# in bash we could equate this to 'echo $mh | xxd -p -r | base64'
 print (binascii.b2a_base64((binascii.unhexlify(mh))))
